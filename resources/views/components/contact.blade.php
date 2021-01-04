f5a4568e26b4d80238f0baac09f0030124cd9721
sarah <s.lindner@mein.gmx>
2021-01-04 14:46:38 +0100

    update experience and profile

--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 sm:grid-cols-3 col-gap-6 sm:col-gap-8 md:col-gap-10 lg:col-gap-12 xl:col-gap-16 row-gap-6">
 	<h1 class="col-span-1 sm:row-span-2 flex items-center justify-center text-gray-200 font-bold text-right text-3xl my-4 sm:m-0">Contact</h1>
-	<p class="col-span-1 sm:col-span-2 text-gray-200"><a href="https://www.xing.com/profile/Sarah_Lindner40" class="underline">Xing Profile</a></p>
-	<p class="col-span-1 sm:col-span-2 text-gray-200">s.lindner@mein.gmx</p>
+	<p class="col-span-1 sm:col-span-2 text-gray-200"><a href="https://www.xing.com/profile/Sarah_Lindner40" class="underline">Xing</a></p>
+	<p class="col-span-1 sm:col-span-2 text-gray-200"><a href="https://github.com/SaraLindner" class="underline">Github</a></p>
 </div>
--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,5 +1,11 @@
 <div class="grid grid-cols-1 sm:grid-cols-3 col-gap-6 sm:col-gap-8 md:col-gap-10 lg:col-gap-12 xl:col-gap-16 row-gap-6">
 	<h1 class="col-span-1 sm:row-span-2 flex items-center justify-center text-gray-200 font-bold text-right text-3xl my-4 sm:m-0">Profile</h1>
 	<p class="col-span-1 sm:col-span-2 text-gray-200">Hello, I am Sarah Lindner.</p>
-	<p class="text-gray-200">Did you ever experienced a webpage which design was so bad you could not find what you were searching for? Me too. Curious about the characteristics that makes a webpage user-friendly, I started to teach myself HTML and CSS. Currently I am exploring the Tailwind and Laravel frameworks.</p>
+	<p class="text-gray-200">Did you ever experienced a webpage which design was so bad you could not find what you were searching for? 
+		Me too! Curious about the characteristics that makes a webpage user-friendly, 
+		I started to teach myself HTML and CSS. Currently I am not only exploring the Tailwind and Laravel frameworks but also getting into Java. <br><br>
+		Do you want to check out my projects? <br>
+		Go to <a href="https://github.com/SaraLindner" class="underline">Github.</a>
+	</p>
+	
 </div>
